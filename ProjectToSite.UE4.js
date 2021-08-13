46a42348bdf922c0c24d1007efbca3e4e746af54
AlexDemas <88877603+AlexDemas@users.noreply.github.com>
2021-08-13 11:35:43 +0300

    Update ProjectToSite.UE4.js

--- ProjectToSite.UE4.js
+++ ProjectToSite.UE4.js
@@ -1154,7 +1154,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MORE JS
-		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('ProjectToSite.data.js'));
+		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/AlexDemas/JapanDrive-UE4-v.01/603225ef7f8f0a7e872569088136e3a1fc5b0963/ProjectToSite.data.js'));
 		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
